dd8d77a3c06c97e4dc49b28dce6bae136f69c7fe
TJ Usiyan <griotspeak@gmail.com>
2013-03-08 14:52:55 -0500

    create a header, not a class, for the framework.

--- ___PACKAGENAMEASIDENTIFIER___.h
+++ ___PACKAGENAMEASIDENTIFIER___.h
@@ -1,13 +1,2 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import <Foundation/Foundation.h>
 
-@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
-
-@end
